f45694119a2bff20658006df9056ad7e7a795b2c
Dethekar <dethekar@gmail.com>
2021-07-14 23:26:44 +0100

    initial commit

---
+++ server.py
@@ -0,0 +1,11 @@
+# server.py
+from bottle import route, run
+from predict import *
+
+
+@route('/<input_line>')
+def index(input_line):
+    return {'result': predict(input_line, 6)}
+
+run(host='localhost', port=5533)
+
